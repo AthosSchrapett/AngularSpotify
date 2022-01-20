e25c3f0d0c2fb8e8c58308f48d2e0d5d26feace5
Athos Schrapett <athosschrapett@outlook.com>
2022-01-19 22:02:34 -0300

    Inclusão guard e Interface de usuário

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { AutenticadoGuard } from './Guards/autenticado.guard';
 import { Routes } from "@angular/router";
 
 export const AppRotas: Routes = [
@@ -12,6 +13,7 @@ export const AppRotas: Routes = [
   },
   {
     path: 'player',
-    loadChildren: () => import('./pages/player/player.module').then(x => x.PlayerModule)
+    loadChildren: () => import('./pages/player/player.module').then(x => x.PlayerModule),
+    canLoad: [AutenticadoGuard]
   }
 ]
