e8c796d0167d60b7883a7ca4a1dba2314bd83b4b
Athos Schrapett <athosschrapett@outlook.com>
2022-01-19 22:18:53 -0300

    validações de login

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -2,6 +2,7 @@ import { SpotifyConfiguration } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 
 import Spotify from 'spotify-web-api-js';
+import { IUsuario } from '../interfaces/IUsuario';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,38 @@ import Spotify from 'spotify-web-api-js';
 export class SpotifyService {
 
   spotifyApi: Spotify.SpotifyWebApiJs = null;
+  usuario: IUsuario;
 
   constructor() {
     this.spotifyApi = new Spotify();
-   }
+  }
 
-  obterUrlLogin(){
+  async inicializarServico() {
+    if (!!this.usuario)
+      return true;
+
+    const token = localStorage.getItem("token");
+
+    if (!!token)
+      return false
+
+    try {
+      this.definirAccessToken(token);
+      await this.obterSpotifyUser();
+      return true
+
+    } catch (ex) {
+      return false;
+
+    }
+  }
+
+  async obterSpotifyUser() {
+    const userInfo = await this.spotifyApi.getMe();
+    console.log(userInfo)
+  }
+
+  obterUrlLogin() {
     const authEndpoint = `${SpotifyConfiguration.authEndpoint}`;
     const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
     const redirectUrl = `redirect_uri=${SpotifyConfiguration.redirectUrl}&`;
@@ -23,14 +50,14 @@ export class SpotifyService {
     return authEndpoint + clientId + redirectUrl + scopes + responseType;
   }
 
-  obterTokenUrlCallback(){
-    if(!window.location.hash)
+  obterTokenUrlCallback() {
+    if (!window.location.hash)
       return '';
     const params = window.location.hash.substring(1).split('&');
     return params[0].split('=')[1];
   }
 
-  definirAccessToken(token: string){
+  definirAccessToken(token: string) {
     this.spotifyApi.setAccessToken(token);
     localStorage.setItem('token', token);
   }
